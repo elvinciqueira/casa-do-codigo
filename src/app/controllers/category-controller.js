7a0606a043b6fde37041a6c3856736a91df176bc
elvinciqueira <elvinciqueira@gmail.com>
2020-11-30 17:04:45 -0300

    feat: nome não pode ser duplicado

--- src/app/controllers/category-controller.js
+++ src/app/controllers/category-controller.js
@@ -3,8 +3,14 @@ import Category from '../models/category'
 async function createCategory(req, res) {
   const { name } = req.body
 
+  const existingCategory = await Category.findOne({ where: { name }})
+
   if (!name) { 
     return res.status(400).json({ error: 'name cannot be blank '})
+  }
+
+  if (existingCategory) {
+    return res.status(400).json({ error: 'Category already exists'})
   }
 
   const category = await Category.create({ name })
