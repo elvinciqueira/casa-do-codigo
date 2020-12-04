80de81a8d84da7a12d7cba84aba440f226aed6dd
elvinciqueira <elvinciqueira@gmail.com>
2020-12-04 16:57:26 -0300

    extrair função

--- src/app/controllers/order-controller.js
+++ src/app/controllers/order-controller.js
@@ -20,48 +20,56 @@ async function registerOrder(req, res) {
     order,
   } = req.body
 
-  const books = await bookDB.find()
   const existingCountry = await countryDB.readById(country_id)
-
+  
   if (existingCountry.state && !state) {
     return res.status(400).json({ error: 'state cannot be blank'})
   }
-
+  
   order.itens.forEach(({ quantity }) => {
     if (!quantity ||quantity < 0) {
       return res.status(400).json({ error: 'quantity must be greater than 0'})
     }
   })
+  
+  let quantity = calculateOrderQuantity(order)
+  let purchasePrice = await calculateTotalPurchase(order, quantity)
 
-  let quantity = 
-    order.itens
-    .map(item => item.quantity)
-    .reduce((acc, value) => acc + value)
-
-  let price = 0
-
-  books.filter((book, index) => {
-    if (book.id === order.itens[index].idBook) {
-      price = book.price * quantity
-    }
-  })
-
-  if (total > price) {
+  if (total > purchasePrice) {
     return res.status(400).json({ error: 'total must be less than all book price'})
   }
 
-  const { id } = await orderDB.insert({ 
-    discount_id,
-    total, 
-    itens: order.itens,
-  })
+  // const { id } = await orderDB.insert({ 
+  //   discount_id,
+  //   total, 
+  //   itens: order.itens,
+  // })
 
-  await purchaseDB.insert({ 
-    order_id: id,
-    ...req.body
-  })
+  // await purchaseDB.insert({ 
+  //   order_id: id,
+  //   ...req.body
+  // })
 
   return res.status(201).json({ orderId: 'endereco-compra'})
+}
+
+function calculateOrderQuantity(aOrder) {
+  return aOrder.itens
+      .map(item => item.quantity)
+      .reduce((acc, value) => acc + value)
+}
+
+async function calculateTotalPurchase(aOrder, aQuantity) {
+  const books = await bookDB.find()
+  let price = 0
+
+  books.filter((book, index) => {
+    if (book.id === aOrder.itens[index].idBook) {
+      price = book.price * aQuantity
+    }
+  })
+
+  return price
 }
 
 
